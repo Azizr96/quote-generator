6f150f6947592f4b93b482ec72004c32cac76d42
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-06-16 22:16:49 +0100

    DOM Interaction added to generate quote and tweet with buttons

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authortext = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
 let apiQuotes = [];
+
 /*
 //generate quote locally
+
 function newQuote (){
  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
 
@@ -17,9 +24,24 @@ newQuote()
 function newQuote (){
  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
- console.log(quote)
+ // check if author field is blank and replac with unknown
 
-} 
+ if(!quote.author){
+    authortext.textContent = 'Unknown';
+ } else {
+  authortext.textContent = quote.author;
+ };
+ // check quote length to determine styligng
+
+ if ( quote.text.length > 50){
+  quoteText.classList.add('long-quote')
+ } else {
+  quoteText.classList.remove('long-quote')
+ }
+
+ quoteText.textContent = quote.text;
+
+} ;
 
 // Get Quotes from API
 async function getQuotes() {
@@ -37,4 +59,17 @@ async function getQuotes() {
   };
 };
 
+//load on page open
 getQuotes();
+
+// tweet quote
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authortext.textContent}`;
+  window.open(twitterUrl, '_blank');
+};
+
+//Event Listerns
+
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
