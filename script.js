7339a46779a1d0e61b6ddaa73b2b934c1bf6680e
Rauhan Aziz <rauhan_aziz@yahoo.co.uk>
2026-06-16 21:45:26 +0100

    generate randome code from api

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+let apiQuotes = [];
+
+
+
+
+
+
+
+//This is if you want to get from API
+//Show new Quote
+function newQuote (){
+ const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+ console.log(quote)
+
+} 
+/
+// Get Quotes from API
+async function getQuotes() {
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+  try {
+    const response = await fetch(apiUrl);
+
+    apiQuotes = await response.json();
+    newQuote();
+
+  } catch(error){
+    // Catch Error here
+
+  };
+};
+
+getQuotes();
